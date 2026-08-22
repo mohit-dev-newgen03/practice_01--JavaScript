2d212effc273ff3e5d5d58ee0b281a04e7293519
Mohit verma <mohitvermadev03@gmail.com>
2026-08-22 22:40:59 +0530

    script JS Task 3 : Numbers — getRandomBetween

--- JavaScript_Fundamentals-001/04-Data_Types_(Advanced)/02_Numbers( Advanced )/script.js
+++ JavaScript_Fundamentals-001/04-Data_Types_(Advanced)/02_Numbers( Advanced )/script.js
@@ -24,3 +24,13 @@ function isSafeToDisplay (price) {
 console.log(isSafeToDisplay(299));              // true
 console.log(isSafeToDisplay(1/0));              // false  = because the sum of a such value is infinity 
 console.log(isSafeToDisplay(0/0));              // false  = because the sum of a such value is NaN 
+
+
+// Task 3 : Numbers — getRandomBetween
+
+
+function getRandomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomBetween(1, 6));
