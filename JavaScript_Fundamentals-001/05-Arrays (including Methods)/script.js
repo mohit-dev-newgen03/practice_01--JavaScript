52f519224b2dbe978cd9c93d20d1d4807bac9836
Mohit verma <mohitvermadev03@gmail.com>
2026-09-02 07:05:57 +0530

    ARRAYS, script JS Task 3 : Array basics (push/pop + array mutability)

--- JavaScript_Fundamentals-001/05-Arrays (including Methods)/script.js
+++ JavaScript_Fundamentals-001/05-Arrays (including Methods)/script.js
@@ -23,3 +23,16 @@ for (let i = 0; i < dailyAttendence.length; i++){
 
 console.log(total);                                             // 275 = Total of the given attendence
 console.log(dailyAttendence[dailyAttendence.length - 1]);       // 55 = Last value of the attendence array 
+
+
+// Task 3 : Array basics (push/pop + array mutability)
+
+
+let todoList = ["Buy groceries", "Clean house", "Pay bills"];
+
+todoList.push("Call dentist");
+
+let undoneTask = todoList.pop();    // Removing the last task and storing it in a new undoneTask variable
+
+console.log(undoneTask);            // The task that were removed 
+console.log(todoList);              // Whole todo list 
