7ea5d27c1dc8d491f7fcabf5bc6006087d42d2d1
Mohit verma <mohitvermadev03@gmail.com>
2026-09-02 06:49:50 +0530

    ARRAYS script JS Task 2 : Array basics (indexed loop + length)

--- JavaScript_Fundamentals-001/05-Arrays (including Methods)/script.js
+++ JavaScript_Fundamentals-001/05-Arrays (including Methods)/script.js
@@ -9,3 +9,17 @@ printQueue.push("Photo.png");
 
 console.log(printQueue.shift());            // Report.pdf
 console.log(printQueue);                    // (2) ['Invoice.docx', 'Photo.png']
+
+
+// Task 2 : Array basics (indexed loop + length)
+
+
+let dailyAttendence = [45, 62, 38, 71, 55];
+
+let total = 0;
+for (let i = 0; i < dailyAttendence.length; i++){
+    total += dailyAttendence[i];  
+};
+
+console.log(total);                                             // 275 = Total of the given attendence
+console.log(dailyAttendence[dailyAttendence.length - 1]);       // 55 = Last value of the attendence array 
