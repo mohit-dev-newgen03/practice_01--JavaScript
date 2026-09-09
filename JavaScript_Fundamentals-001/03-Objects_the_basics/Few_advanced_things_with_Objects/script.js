0a0c72e100a28125f36f7a16f55e21044bf568b8
Mohit verma <mohitvermadev03@gmail.com>
2026-09-09 22:17:08 +0530

    Task 3 : Objects Advanced (Object.entries())

--- JavaScript_Fundamentals-001/03-Objects_the_basics/Few_advanced_things_with_Objects/script.js
+++ JavaScript_Fundamentals-001/03-Objects_the_basics/Few_advanced_things_with_Objects/script.js
@@ -16,3 +16,12 @@ let salesByCategory = { electronics: 45000, clothing: 12000, groceries: 8000 };
 let totalRevenue = Object.values(salesByCategory).reduce((total, rightNow) => total + rightNow, 0);
 
 console.log(totalRevenue);                   
+
+
+// Task 3 : Objects Advanced (Object.entries())
+
+let stockCounts = { pens: 2, notebooks: 0, erasers: 15, staplers: 0 };
+
+let outOFstocks = Object.entries(stockCounts).filter(empty => empty[1] === 0);
+
+console.log(outOFstocks);
