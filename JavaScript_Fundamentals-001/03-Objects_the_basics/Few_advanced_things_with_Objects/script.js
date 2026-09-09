3c4fd69e90636635363f9c5514b0657542c8a1e7
Mohit verma <mohitvermadev03@gmail.com>
2026-09-09 20:48:42 +0530

    Task 2 : Objects Advanced (Object.values())

--- JavaScript_Fundamentals-001/03-Objects_the_basics/Few_advanced_things_with_Objects/script.js
+++ JavaScript_Fundamentals-001/03-Objects_the_basics/Few_advanced_things_with_Objects/script.js
@@ -7,3 +7,12 @@ let keys = Object.keys(formData);
     keys.forEach((element) => {
         console.log(`${element} : ${formData[element]}`);
     });
+
+
+// Task 2 : Objects Advanced (Object.values()) 
+
+let salesByCategory = { electronics: 45000, clothing: 12000, groceries: 8000 };
+
+let totalRevenue = Object.values(salesByCategory).reduce((total, rightNow) => total + rightNow, 0);
+
+console.log(totalRevenue);                   
