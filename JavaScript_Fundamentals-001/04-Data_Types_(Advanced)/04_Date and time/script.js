49259cff9efa702e5d4a4e52574863307caf14a9
Mohit verma <mohitvermadev03@gmail.com>
2026-09-15 23:43:59 +0530

    Date and time Task 2 : Calculating : time ago (date differences)

--- JavaScript_Fundamentals-001/04-Data_Types_(Advanced)/04_Date and time/script.js
+++ JavaScript_Fundamentals-001/04-Data_Types_(Advanced)/04_Date and time/script.js
@@ -8,3 +8,12 @@ let month = (dateAndTime.getMonth());
 let date = (dateAndTime.getDate());
 
 console.log(`Posted on : ${date}/${month + 1}/${year}`)         // Posted on : 12/9/2026
+
+
+// Task 2 : Calculating : time ago (date differences)
+
+let commentTime = new Date("2026-09-15T10:00:00");
+let now = new Date("2026-09-15T10:45:00"); 
+
+let timeDifference = (((now - commentTime) / 1000 ) /60 ) ;
+console.log(`Posted ${timeDifference} minutes ago`);                // Posted 45 minutes ago
